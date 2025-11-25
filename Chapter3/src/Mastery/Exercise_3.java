2edb8742dc8230ff906537e1ae06e28c0693cebb
510118003 <510118003@L815WD220026.edu.cbe.ab.ca>
2025-11-25 11:05:21 -0700

    11:05 AM

---
+++ Chapter3/src/Mastery/Exercise_3.java
@@ -0,0 +1,18 @@
+package Mastery;
+
+public class Exercise_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("***************");
+		System.out.format("%1s %13s %n", "*", "*");
+		System.out.format("%1s %13s %n", "*", "*");
+		System.out.format("%1s %13s %n", "*", "*");
+		System.out.format("%1s %13s %n", "*", "*");
+		System.out.format("%1s %13s %n", "*", "*");
+		System.out.format("%1s %13s %n", "*", "*");
+		System.out.format("%1s %13s %n", "*", "*");
+		System.out.println("***************");
+	}
+
+}
---
+++ Chapter3/src/Mastery/Exercise_6.java
@@ -0,0 +1,17 @@
+package Mastery;
+
+public class Exercise_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Tic Tac Toe Rules");
+		System.out.println("Tic Tac Toe is a game for two players");
+		System.out.println("A grid of nine sqaures is drawn");
+		System.out.println("Player one draws an X i an empty square");
+		System.out.println("Player two draws an O in an empty square");
+		System.out.println("Steps 2 and three are repeated until a tic tac pattern is created, patterns can be horizontal, diaganol or vetical");
+		System.out.println("A winning pattern is completed by drawing a line through the pattern");
+	}
+
+}
