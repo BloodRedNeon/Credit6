eb5044dae8d69faeafcb07679e494299008eb786
510118003 <510118003@L815WD220026.edu.cbe.ab.ca>
2025-11-26 09:08:25 -0700

    9:09AM

--- Chapter3/src/Mastery/Exercise_6.java
+++ Chapter3/src/Mastery/Exercise_6.java
@@ -5,13 +5,13 @@ public class Exercise_6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Tic Tac Toe Rules");
-		System.out.println("Tic Tac Toe is a game for two players");
-		System.out.println("A grid of nine sqaures is drawn");
-		System.out.println("Player one draws an X i an empty square");
-		System.out.println("Player two draws an O in an empty square");
-		System.out.println("Steps 2 and three are repeated until a tic tac pattern is created, patterns can be horizontal, diaganol or vetical");
-		System.out.println("A winning pattern is completed by drawing a line through the pattern");
+		System.out.println("1. Tic Tac Toe Rules");
+		System.out.println("2. Tic Tac Toe is a game for two players");
+		System.out.println("3. A grid of nine sqaures is drawn");
+		System.out.println("4. Player one draws an X i an empty square");
+		System.out.println("5. Player two draws an O in an empty square");
+		System.out.println("6. Steps 2 and three are repeated until a tic tac pattern is created, patterns can be horizontal, diaganol or vetical");
+		System.out.println("7. A winning pattern is completed by drawing a line through the pattern");
 	}
 
 }
